01fca3c275c836f9e810fd268771dc3bd8c4337b
lourenco <otavioluissv@gmail.com>
2023-02-05 22:01:02 +0000

    last update

--- JS/formOrcamento.js
+++ JS/formOrcamento.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('.popup-with-form').magnificPopup({
 		type: 'inline',
 		preloader: false,
-		focus: '#name',
+		focus: '#nome',
 
 		callbacks: {
 			beforeOpen: function() {
 				if($(window).width() < 700) {
 					this.st.focus = false;
 				} else {
-					this.st.focus = '#name';
+					this.st.focus = '#nome';
 				}
 			}
 		}
